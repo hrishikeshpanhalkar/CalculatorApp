e1d2ec8c905abe13214308ab1d2cdb26b4c481be
Hrishikesh Panhalkar <hrishikeshpanhalkar07@gmail.com>
2021-03-17 20:13:48 +0530

    First Commit

---
+++ app/src/main/java/com/example/calculator/HomeActivity.java
@@ -0,0 +1,191 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+    TextView answer_tv;
+    Button zeroBtn, oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn, allClearBtn,
+            answerBtn, backBtn, addBtn, subBtn, mulBtn, divBtn, dotBtn;
+    String result;
+    String tmp;
+    String operator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home);
+
+        answer_tv = (TextView) findViewById(R.id.answer_tv);
+        zeroBtn = (Button) findViewById(R.id.zeroBtn);
+        oneBtn = (Button) findViewById(R.id.oneBtn);
+        twoBtn = (Button) findViewById(R.id.twoBtn);
+        threeBtn = (Button) findViewById(R.id.threeBtn);
+        fourBtn = (Button) findViewById(R.id.fourBtn);
+        fiveBtn = (Button) findViewById(R.id.fiveBtn);
+        sixBtn = (Button) findViewById(R.id.sixBtn);
+        sevenBtn = (Button) findViewById(R.id.sevenBtn);
+        eightBtn = (Button) findViewById(R.id.eightBtn);
+        nineBtn = (Button) findViewById(R.id.nineBtn);
+        allClearBtn = (Button) findViewById(R.id.allclearbtn);
+        answerBtn = (Button) findViewById(R.id.answerBtn);
+        backBtn = (Button) findViewById(R.id.backBtn);
+        addBtn = (Button) findViewById(R.id.additionBtn);
+        subBtn = (Button) findViewById(R.id.subtractBtn);
+        mulBtn = (Button) findViewById(R.id.multiplicationBtn);
+        divBtn = (Button) findViewById(R.id.divisionBtn);
+        dotBtn = (Button) findViewById(R.id.dotBtn);
+
+        zeroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("0");
+            }
+        });
+        oneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("1");
+            }
+        });
+        twoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("2");
+            }
+        });
+        threeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("3");
+            }
+        });
+        fourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("4");
+            }
+        });
+        fiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("5");
+            }
+        });
+        sixBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("6");
+            }
+        });
+        sevenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("7");
+            }
+        });
+        eightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("8");
+            }
+        });
+        nineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked("9");
+            }
+        });
+        allClearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClearButtonClicked();
+            }
+        });
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                onOperatorButtonClicked("-");
+            }
+        });
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("+");
+            }
+        });
+        mulBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("X");
+            }
+        });
+        divBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("/");
+            }
+        });
+        answerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEqualButtonClicked();
+            }
+        });
+        dotBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberButtonClicked(".");
+            }
+        });
+    }
+    private void onEqualButtonClicked() {
+        float res = 0;
+        try {
+            float number = Float.parseFloat(tmp);
+            float number2 =  Float.parseFloat(answer_tv.getText().toString());
+            switch (operator) {
+                case "+":
+                    res = number + number2;
+                    break;
+                case "/":
+                    res = number / number2;
+                    break;
+                case "-":
+                    res = number - number2;
+                    break;
+                case "X":
+                    res = number * number2;
+                    break;
+            }
+            result = String.valueOf(res);
+            answer_tv.setText(result);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void onOperatorButtonClicked(String operator) {
+        tmp = answer_tv.getText().toString();
+        onClearButtonClicked();
+        this.operator = operator;
+    }
+
+    private void onClearButtonClicked() {
+        result = "";
+        answer_tv.setText("");
+    }
+
+    private void onNumberButtonClicked(String pos) {
+        result = answer_tv.getText().toString();
+        result = result + pos;
+        answer_tv.setText(result);
+    }
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Animation bottomAnim;
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        title = (TextView) findViewById(R.id.title);
+
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        title.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+}
